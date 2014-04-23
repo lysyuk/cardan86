5a9b0cfc6150e6cda752c3558b7c9a2ef26fd445
Yury Lysyuk <lysyuk.y@gmail.com>
2014-04-23 12:52:14 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+	$(function(){
+	
+	var note = $('#note');
+		ts = new Date();
+        ts.setHours(ts.getHours() + 22);
+        ts.setMinutes(ts.getMinutes() + 56);
+        ts.setSeconds(ts.getSeconds() + 60);
+		newYear = true;
+	
+	if((new Date()) > ts){
+		ts = (new Date()).getTime() + 10*24*60*60*1000;
+	}
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += "Дней: " + days +", ";
+			message += "часов: " + hours + ", ";
+			message += "минут: " + minutes + " и ";
+			message += "секунд: " + seconds + " <br />";
+			
+			
+			note.html(message);
+		}
+	});
+});
